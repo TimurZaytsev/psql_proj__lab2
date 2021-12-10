302f38e49d5ab5eb811db89d8d7cb51f9c1adbc5
kasyanikita <71206801+kasyanikita@users.noreply.github.com>
2021-12-10 22:49:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,14 @@ from PyQt5 import Qt as Q
 from gui import Ui
 
 
+class MainWindow(Q.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui()
+        self.ui.setupUi(self)
+
 if __name__ == '__main__':
-    database = db.DatabasePy()
-    #database.create_db("testing")
-    #database.test_1_step()
-    #database.drop_db()
-
-
-    class MainWindow(Q.QMainWindow):
-        def __init__(self):
-            super().__init__()
-            self.ui = Ui()
-            self.ui.setupUi(self)
-
-    if __name__ == '__main__':
-        app = Q.QApplication(sys.argv)
-        w = MainWindow()
-        w.show()
-        sys.exit(app.exec_())
+    app = Q.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
